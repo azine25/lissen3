af87d92d6d54fd23a3269fee15afc68271df259a
azine25 <azine.maratova@gmail.com>
2021-02-27 17:23:07 +0600

    Полностью изменила.

--- src/com/Main.java
+++ src/com/Main.java
@@ -7,25 +7,38 @@ public class Main {
     public static void main(String[] args) {
         double nums[] = {1.0, -7.3, 4.3, -2.0, 2.3, -1.2, 3.2, -3.7, 3.4, 2.0, 1.9, -4.1, 0.3, -1.3, 4.0};
 
-        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            System.out.println(Arrays.toString(nums));
+            for (int j = nums.length - 1; j > i; j--) {
+                if (nums[j - 1] > nums[j]) {
+                    double tmp = nums[j - 1];
+                    nums[j - 1] = nums[j];
+                    nums[j] = tmp;
+                }
 
 
-        double sum = 0.0;
-        int amount = 0;
-        boolean isNegotiv = false;
-
-        for (double num : nums) {
-            if (num < 0) {
-                isNegotiv = true;
-            } else if (isNegotiv) {
-                sum +=num;
-                amount++;
             }
         }
-        double averageSum = sum / amount;
-        System.out.println("Среднее арифмитическое значение положительних чисел "+averageSum);
-        System.out.println(Arrays.toString(nums));
-    }
+
+
+
+                double sum = 0.0;
+                int amount = 0;
+                boolean isNegotiv = false;
+
+                for (double num : nums) {
+                    if (num < 0) {
+                        isNegotiv = true;
+                    } else if (isNegotiv) {
+                        sum += num;
+                        amount++;
+                    }
+                }
+                double averageSum = sum / amount;
+                System.out.println("Среднее арифмитическое значение положительних чисел " + averageSum);
+
+            }
+
 
     /*
     Создать массив дробных (не целых) чисел размером 15 элементов и наполнить его положительными и отрицательными числами;
@@ -40,4 +53,6 @@ public class Main {
     {-4, -2, 2, 3, 6, 8}
     Дэдлайн 27.02.2021 23 59
      */
+
+
 }
